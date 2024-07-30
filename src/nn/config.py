33f2d92e0bcdf8219029dc03920b3903cceadbc8
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2024-07-30 16:37:44 +0700

    Change device selection to cuda or cpu depending on detection

--- src/nn/config.py
+++ src/nn/config.py
@@ -1,3 +1,6 @@
-DEVICE="cuda"
+import torch
+
+
+DEVICE=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 BAR_FORMAT='{desc}: {percentage:3.0f}% {n_fmt}/{total_fmt} [{elapsed}<{remaining}{postfix}]'
 LEARNING_RATE=0.0002
